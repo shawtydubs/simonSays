d8845bcf1db6ed6ceeea19474dec1e0d4efceb59
Claire Walters <claire.walters@opower.com>
2017-03-24 22:32:01 -0400

    refactor and add comparison

--- simon.js
+++ simon.js
@@ -1,20 +1,21 @@
-var colors = [
-    { name: '#green',  sound: 'sounds/green.mp3' },
-    { name: '#red',    sound: 'sounds/red.mp3' },
-    { name: '#yellow', sound: 'sounds/yellow.mp3' },
-    { name: '#blue',   sound: 'sounds/blue.mp3' }
-]
+var colors = {
+    1: { name: '.green',  sound: 'sounds/green.mp3' },
+    2: { name: '.red',    sound: 'sounds/red.mp3' },
+    3: { name: '.yellow', sound: 'sounds/yellow.mp3' },
+    4: { name: '.blue',   sound: 'sounds/blue.mp3' }
+}
 
 var count = 0;
-// var sequence = [];
+var gameColors = [];
+var playerDots = [];
 
 // For testing
-var sequence = [
-    { name: '#green',  sound: 'sounds/green.mp3' },
-    { name: '#red',    sound: 'sounds/red.mp3' },
-    { name: '#yellow', sound: 'sounds/yellow.mp3' },
-    { name: '#blue',   sound: 'sounds/blue.mp3' }
-];
+// var gameColors = [
+//     { name: '.green',  sound: 'sounds/green.mp3' },
+//     { name: '.red',    sound: 'sounds/red.mp3' },
+//     { name: '.yellow', sound: 'sounds/yellow.mp3' },
+//     { name: '.blue',   sound: 'sounds/blue.mp3' }
+// ];
 
 updateCount();
 
@@ -23,32 +24,66 @@ $('button').click(function() {
 });
 
 $('#start').click(function() {
-    startGame();
+    startRound();
 });
+
+$('.dot').click(function() {
+    playerDots.push(colors[$(this).attr('id')]);
+    console.group('Player dots:');console.log(playerDots);console.groupEnd();
+    playerBeep($(this).attr('id'));
+    compareDots();
+})
 
 function updateCount() {
     $('#count-tally').html(count);
 }
 
-function startGame() {
-    // sequence.push(colors[Math.floor(Math.random() * 4)]);
-    console.log(sequence);
+function startRound() {
+    gameColors.push(colors[Math.floor(Math.random() * 4) + 1]);
+    console.group('Game colors:');console.log(gameColors);console.groupEnd();
     count++;
     updateCount();
-    makeItBeep(sequence);
+    gameBeep();
 }
 
-function makeItBeep(colors) {
+function playerBeep(id) {
+    console.log('Player beep');
+    $('#' + id).addClass('selected');
+    new Audio(colors[id].sound).play();
+    setTimeout(function() {
+        $('#' + id).removeClass('selected');
+    }, 500); 
+}
+
+function gameBeep() {
+    console.log('Starting game sequence');
     var i = 0;
     (function beepBeep() {
-        $(colors[i].name).addClass('selected');
-        new Audio(colors[i].sound).play();
+        $(gameColors[i].name).addClass('selected');
+        new Audio(gameColors[i].sound).play();
         setTimeout(function() {
-            $(colors[i].name).removeClass('selected');
-            if (++i < colors.length) {
+            $(gameColors[i].name).removeClass('selected');
+            if (++i < gameColors.length) {
                 setTimeout(beepBeep, 500);
             }
         }, 500); 
     })();
+}
+
+function compareDots() {
+    for (i = 0; i < playerDots.length; i++) {
+        if (gameColors[i].name == playerDots[i].name) {
+            console.log('Colors are the same!');
+        } else {
+            console.log('Colors are different');
+        }
+    }
+    startRound();
+}
+
+function resetGame() {
+    gameColors = [];
+    playerDots = [];
+    count = 0;
 }
 
